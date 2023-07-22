8e90cded134234d24549dc4ec012aa10f46bfdf0
Aspect-6 <neelpatel246624@gmail.com>
2023-07-22 10:44:07 -0400

    Removed extra code

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ module.exports = {
     //Functions
     user: require('@lib/functions/user'),
 
-    //React
-    React: require('react'),
-
     //Shorten code
     getElement: require('@utils/shorten/getElement'),
 }
+
 //Globalize exported data
 for (const key in module.exports) global[key] = module.exports[key];
 
