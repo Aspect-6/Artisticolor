c9be9a2d896f7f03d035fba42d259b0d9cd0729d
Aspect-6 <neelpatel246624@gmail.com>
2023-07-22 11:48:03 -0400

    changed how login function works for bootstrap

--- src/components/functions/login.tsx
+++ src/components/functions/login.tsx
@@ -1,26 +1,22 @@
 import { useEffect, useState } from 'react'
-import { renderHeader } from './renderHeader'
+// import { renderHeader } from './renderHeader'
 // import LoginBox from '../LoginBox/LoginBox'
 
 export default function Login() {
+	const [currentAuthState, setCurrentAuthState] = useState(null)
 
-    const [authState, setAuthState] = useState(null)
-    
-    useEffect(() => {
-        //@ts-expect-error
-        onAuthStateChanged(auth, (userData) => {
-            if(userData) {
-                setAuthState(userData)
-                console.log(userData)
-            }
-        })
-    }, [authState])
-    return (
-        <>
-            {renderHeader(location.href, authState)}
-        </>
-    )
+	useEffect(() => {
+		//@ts-expect-error
+		onAuthStateChanged(auth, (userData) => {
+			if (userData) {
+				setCurrentAuthState(userData)
+				console.log(userData)
+			}
+		})
+	}, [currentAuthState])
+	return currentAuthState
+	// (
+	//     <>
+	//     {/* {renderHeader(location.href, currentAuthState)} */}
+	// </>)
 }
-
-//Load Styles
-require('styles/lgnregBox.scss')
