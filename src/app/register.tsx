a386cdd4b32ca45a6f76d5e0e1884afd717590f9
Aspect-6 <neelpatel246624@gmail.com>
2023-07-25 13:18:50 -0400

    Made register page ready for upcoming components

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>	
-		
-	</React.StrictMode>
-);
+	<React.StrictMode></React.StrictMode>
+)
