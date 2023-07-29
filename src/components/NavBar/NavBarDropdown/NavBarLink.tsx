80b5d37c270f41c5e7e5e96999352d34a23f63dd
Aspect-6 <neelpatel246624@gmail.com>
2023-07-29 17:21:50 -0400

    chose better word for the prop

--- src/components/NavBar/NavBarDropdown/NavBarLink.tsx
+++ src/components/NavBar/NavBarDropdown/NavBarLink.tsx
@@ -1,12 +1,12 @@
 interface NavBarLinkProps extends React.AnchorHTMLAttributes<HTMLAnchorElement> {
-	value: string
+	text: string
 }
 
-export default function NavBarLink({ value, ...props }: NavBarLinkProps) {
+export default function NavBarLink({ text, ...props }: NavBarLinkProps) {
 	return (
 		<li className='nav-item'>
 			<a className='nav-link fs-5 px-2' {...props}>
-				{value}
+				{text}
 			</a>
 		</li>
 	)
