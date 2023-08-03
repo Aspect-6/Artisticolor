e55c19e3e6790114e08145fda55822514b252d0a
Aspect-6 <neelpatel246624@gmail.com>
2023-08-03 15:21:55 -0400

    commented to prepare for new component later on

--- src/app/profile.tsx
+++ src/app/profile.tsx
@@ -1,11 +1,11 @@
 import NavBar from '@components/NavBar/NavBar'
-import ProfileBox from '@components/ProfileBox/ProfileBox'
+// import ProfileBox from '@components/ProfileBox/ProfileBox'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<NavBar />
-		<ProfileBox />
+		{/* <ProfileBox /> */}
 	</React.StrictMode>
 )
--- src/components/ProfileBox/ProfileBox.tsx
+++ src/components/ProfileBox/ProfileBox.tsx
@@ -1,16 +1,16 @@
-import InputWrap from "./InputWrap"
+// import InputWrap from "./InputWrap"
 
-export default function ProfileBox() {
-    return (
-        <div id="userData" className="accountBox">
-            <h2 id="header"></h2>
+// export default function ProfileBox() {
+//     return (
+//         <div id="userData" className="accountBox">
+//             <h2 id="header"></h2>
 
-            <form id="changeData" onSubmit={(e) => e.preventDefault()}>
-                <InputWrap type="Email" />
-                <InputWrap type="Username" />
-                <InputWrap type="Password" />
-                <button type="submit" id="logout">Log Out</button>
-            </form>
-        </div>
-    );
-};
+//             <form id="changeData" onSubmit={(e) => e.preventDefault()}>
+//                 <InputWrap type="Email" />
+//                 <InputWrap type="Username" />
+//                 <InputWrap type="Password" />
+//                 <button type="submit" id="logout">Log Out</button>
+//             </form>
+//         </div>
+//     );
+// };
