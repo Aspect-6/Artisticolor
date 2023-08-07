5b0ef978a187487f7f9fa18dcf2741eeefbbfc22
Aspect-6 <neelpatel246624@gmail.com>
2023-08-07 12:59:01 -0400

    added another type

--- src/lib/types/auth/index.d.ts
+++ src/lib/types/auth/index.d.ts
@@ -1,9 +1,13 @@
 /**
  * User which gets stored in firestore database
-*/
+ */
 interface FirestoreUser {
     email: string
     username: string
     password: string
     key: string
 }
+/**
+ * Keys of `FirestoreUser` interface allowed to be decrypted with `Crypto.decrypt()` utility function
+ */
+type FirestoreUserValidKeys = Omit<FirestoreUser, "key">
