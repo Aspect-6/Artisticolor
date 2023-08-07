4fc60c2e572fbe4dcbe9d0144f139074abb362d4
Aspect-6 <neelpatel246624@gmail.com>
2023-08-07 14:24:07 -0400

    removed extra files

--- src/lib/functions/database.ts
+++
@@ -1,6 +0,0 @@
-import { getDatabase } from '@firebase/database'
-import { app } from './app'
-
-export const db = getDatabase(app)
-
-export { get, getDatabase, ref, set } from '@firebase/database'
--- src/lib/utilities/firebase/getData.ts
+++
@@ -1,6 +0,0 @@
-import { db, get, ref } from '@lib/functions/database'
-
-module.exports = async (uid: string) => [
-	(await get(ref(db, `Users/${uid}/Password`))).val(),
-	(await get(ref(db, `Users/${uid}/Key`))).val(),
-]
