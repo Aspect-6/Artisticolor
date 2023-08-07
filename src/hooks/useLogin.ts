439140f0088a5873e571f4a2352de9b2dba0dd3b
Aspect-6 <neelpatel246624@gmail.com>
2023-08-07 14:48:40 -0400

    fixed variable type from being any

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,9 +1,10 @@
-import { auth, onAuthStateChanged } from '@lib/functions/auth'
+import { auth } from '@lib/functions/firebase'
+import { onAuthStateChanged } from '@lib/functions/auth'
 import { useEffect, useState } from 'react'
 import { User } from '@firebase/auth'
 
 export default function useLogin() {
-	const [currentUser, setCurrentUser] = useState(null)
+	const [currentUser, setCurrentUser] = useState<User | null>(null)
 
 	useEffect(() => {
 		onAuthStateChanged(auth, (userData) => {
