79b55149b73f1086b20ff164c61b5cd3bbacd5f3
Aspect-6 <neelpatel246624@gmail.com>
2023-08-06 21:02:49 -0400

    fixed a typo causing incorrect scss bundle

--- src/import_bundles/styles/projects.ts
+++ src/import_bundles/styles/projects.ts
@@ -1,5 +1,5 @@
 module.exports = {
 	...require('bootstrap/scss/bootstrap.scss'),
 	...require('bootstrap/dist/js/bootstrap.bundle.js'),
-	...require('@styles/register.scss'),
+	...require('@styles/projects.scss'),
 }
