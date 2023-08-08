8f423bbf7751e6cde1a57259e9b47df2e1e1c652
Aspect-6 <neelpatel246624@gmail.com>
2023-08-08 08:29:34 -0400

    fixed forwardRef bug

--- src/components/Form/FormError.tsx
+++ src/components/Form/FormError.tsx
@@ -1,13 +1,16 @@
 import { forwardRef } from "react"
 
 interface FormErrorProps extends React.HTMLAttributes<HTMLDivElement> {
-	children: React.ReactNode
+    children: React.ReactNode
 }
 
-export default forwardRef(function FormError({ children, ...props }: FormErrorProps) {
-	return (
-		<div style={{ color: '#dc3545' }} {...props}>
-			{children}
-		</div>
-	)
+export default forwardRef(function FormError(
+    { children, ...props }: FormErrorProps,
+    ref: React.ForwardedRef<HTMLDivElement>
+) {
+    return (
+        <div ref={ref} style={{ color: "#dc3545" }} {...props}>
+            {children}
+        </div>
+    )
 })
--- src/components/Form/FormField/FieldInput.tsx
+++ src/components/Form/FormField/FieldInput.tsx
@@ -2,11 +2,15 @@ import { forwardRef } from "react"
 
 interface FieldInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export default forwardRef(function FieldInput({ ...props }: FieldInputProps) {
-	return (
-		<input
-			{...props}
-			className={'form-control '.concat(props.className)}
-		/>
-	)
+export default forwardRef(function FieldInput(
+    { ...props }: FieldInputProps,
+    ref: React.ForwardedRef<HTMLInputElement>
+) {
+    return (
+        <input
+            ref={ref}
+            {...props}
+            className={"form-control ".concat(props.className)}
+        />
+    )
 })
