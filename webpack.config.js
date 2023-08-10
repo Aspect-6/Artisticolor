be6a2eebe29b94f6227ae0123eab95ff18580896
Aspect-6 <neelpatel246624@gmail.com>
2023-08-10 13:51:50 -0400

    styling from prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,82 @@
-const path = require('path');
-const { readdirSync: readdir } = require('fs')
+const path = require("path")
+const { readdirSync: readdir } = require("fs")
 
 //Plugins
-const HTMLPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const TSConfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const HTMLPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+const TSConfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
 module.exports = {
-
-    mode: 'development',
+    
+    mode: "development",
 
     resolve: {
         plugins: [new TSConfigPathsPlugin()],
-        extensions: ['.tsx', '.ts', '.jsx', '.js']
+        extensions: [".tsx", ".ts", ".jsx", ".js"],
     },
 
     entry: {
-        index: ['./src/app/index', './src/ts/index'],
-        profile: ['./src/app/profile', './src/ts/profile'],
-        projects: ['./src/app/projects', './src/ts/projects'],
-        register: ['./src/app/register', './src/ts/register'],
+        index: ["./src/app/index", "./src/ts/index"],
+        profile: ["./src/app/profile", "./src/ts/profile"],
+        projects: ["./src/app/projects", "./src/ts/projects"],
+        register: ["./src/app/register", "./src/ts/register"],
     },
     //Output files to a dist folder
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "[name].js"
+        filename: "[name].js",
     },
 
     module: {
-        rules: [{
-            test: /\.s[ac]ss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                'css-loader',
-                'sass-loader'
-            ]
-        },
-        {
-            test: /\.jsx?$/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                    },
                 },
             },
-        },
-        {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }]
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
     },
 
     plugins: [
         //Add HTML files
-        ...readdir('./src/pages').filter(file => !file.includes('Icon')).map(file => file.split('.')[0]).map(file => new HTMLPlugin({
-            template: `./src/pages/${file}.html`,
-            inject: true,
-            chunks: [file],
-            filename: `${file}.html`,
-        })),
+        ...readdir("./src/pages")
+            .filter((file) => !file.includes("Icon"))
+            .map((file) => file.split(".")[0])
+            .map(
+                (file) =>
+                    new HTMLPlugin({
+                        template: `./src/pages/${file}.html`,
+                        inject: true,
+                        chunks: [file],
+                        filename: `${file}.html`,
+                    })
+            ),
         //Copy images and icons
         new CopyPlugin({
-            patterns: [{
-                from: 'src/assets/',
-                globOptions: {
-                    ignore: ['**/styles', '**/images', '**/.DS_Store']
+            patterns: [
+                {
+                    from: "src/assets/",
+                    globOptions: {
+                        ignore: ["**/styles", "**/images", "**/.DS_Store"],
+                    },
                 },
-            }]
+            ],
         }),
         //Generate CSS files
-        new MiniCssExtractPlugin({ filename: '[name].css' }),
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
     ],
-};
+}
